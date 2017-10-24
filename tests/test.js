9e3a2936841e4a4d0f9577b8a41a708cf0baeaf0
Kevin Godell <kevin.godell@gmail.com>
2017-10-24 04:22:24 -0500

    changed test to follow change to main

--- tests/test.js
+++ tests/test.js
@@ -17,11 +17,11 @@ let polygonPoints = new PP(vertexes);
 
 assert(polygonPoints.pointsLength === 10000);
 
-assert(polygonPoints.containsPoint({x: 0, y: 0}) === true);
+assert(polygonPoints.containsPoint(0, 0) === true);
 
-assert(polygonPoints.containsPoint({x: 10, y: 10}) === true);
+assert(polygonPoints.containsPoint(10, 10) === true);
 
-assert(polygonPoints.containsPoint({x: 101, y: 0}) === false);
+assert(polygonPoints.containsPoint(101, 0) === false);
 
 assert(polygonPoints.boundingBox[3].x === 100);
 
@@ -37,11 +37,11 @@ polygonPoints.vertexes = vertexes;
 
 assert(polygonPoints.pointsLength === 2550);
 
-assert(polygonPoints.containsPoint({x: 0, y: 0}) === true);
+assert(polygonPoints.containsPoint(0, 0) === true);
 
-assert(polygonPoints.containsPoint({x: 10, y: 10}) === true);
+assert(polygonPoints.containsPoint(10, 10) === true);
 
-assert(polygonPoints.containsPoint({x: 101, y: 0}) === false);
+assert(polygonPoints.containsPoint(101, 0) === false);
 
 assert(polygonPoints.boundingBox[2].y === 50);
 
@@ -57,11 +57,11 @@ polygonPoints.vertexes = vertexes;
 
 assert(polygonPoints.pointsLength === 7500);
 
-assert(polygonPoints.containsPoint({x: 0, y: 0}) === true);
+assert(polygonPoints.containsPoint(0, 0) === true);
 
-assert(polygonPoints.containsPoint({x: 10, y: 10}) === true);
+assert(polygonPoints.containsPoint(10, 10) === true);
 
-assert(polygonPoints.containsPoint({x: 101, y: 0}) === false);
+assert(polygonPoints.containsPoint(101, 0) === false);
 
 assert(polygonPoints.boundingBox[2].y === 100);
 
@@ -77,11 +77,11 @@ console.time('test 4');
 
 assert(polygonPoints.pointsLength === 9184);
 
-assert(polygonPoints.containsPoint({x: 0, y: 0}) === true);
+assert(polygonPoints.containsPoint(0, 0) === true);
 
-assert(polygonPoints.containsPoint({x: 10, y: 10}) === true);
+assert(polygonPoints.containsPoint(10, 10) === true);
 
-assert(polygonPoints.containsPoint({x: 101, y: 0}) === false);
+assert(polygonPoints.containsPoint(101, 0) === false);
 
 assert(polygonPoints.boundingBox[2].y === 150);
 
