f84879ca9d516cae3dd446721997c90fdc8e7f0f
Kevin Godell <kevin.godell@gmail.com>
2017-10-23 21:12:37 -0500

    adjusted tests to match main

--- tests/test.js
+++ tests/test.js
@@ -9,13 +9,13 @@ const PP = require('../index');
 
 /*****************************************************************/
 
+console.time('test 1');
+
 let vertexes = [{x: 0, y: 0}, {x: 100, y: 0}, {x: 100, y: 100}, {x: 0, y: 100}];
 
 let polygonPoints = new PP(vertexes);
 
-console.time('test 1');
-
-assert(polygonPoints.pointsLength() === 10000);
+assert(polygonPoints.pointsLength === 10000);
 
 assert(polygonPoints.containsPoint({x: 0, y: 0}) === true);
 
@@ -23,19 +23,19 @@ assert(polygonPoints.containsPoint({x: 10, y: 10}) === true);
 
 assert(polygonPoints.containsPoint({x: 101, y: 0}) === false);
 
-assert(polygonPoints.boundingBox()[3].x === 100);
+assert(polygonPoints.boundingBox[3].x === 100);
 
 console.timeEnd('test 1');
 
 /*****************************************************************/
 
-vertexes = [{x: 0, y: 0}, {x: 100, y: 0}, {x: 50, y: 50}];
-
-polygonPoints = new PP(vertexes);
-
 console.time('test 2');
 
-assert(polygonPoints.pointsLength() === 2550);
+vertexes = [{x: 0, y: 0}, {x: 100, y: 0}, {x: 50, y: 50}];
+
+polygonPoints.vertexes = vertexes;
+
+assert(polygonPoints.pointsLength === 2550);
 
 assert(polygonPoints.containsPoint({x: 0, y: 0}) === true);
 
@@ -43,19 +43,19 @@ assert(polygonPoints.containsPoint({x: 10, y: 10}) === true);
 
 assert(polygonPoints.containsPoint({x: 101, y: 0}) === false);
 
-assert(polygonPoints.boundingBox()[2].y === 50);
+assert(polygonPoints.boundingBox[2].y === 50);
 
 console.timeEnd('test 2');
 
 /*****************************************************************/
 
-vertexes = [{x: 0, y: 0}, {x: 100, y: 0}, {x: 100, y: 100}, {x: 0, y: 100}, {x: 50, y: 50}];
-
-polygonPoints = new PP(vertexes);
-
 console.time('test 3');
 
-assert(polygonPoints.pointsLength() === 7500);
+vertexes = [{x: 0, y: 0}, {x: 100, y: 0}, {x: 100, y: 100}, {x: 0, y: 100}, {x: 50, y: 50}];
+
+polygonPoints.vertexes = vertexes;
+
+assert(polygonPoints.pointsLength === 7500);
 
 assert(polygonPoints.containsPoint({x: 0, y: 0}) === true);
 
@@ -63,7 +63,7 @@ assert(polygonPoints.containsPoint({x: 10, y: 10}) === true);
 
 assert(polygonPoints.containsPoint({x: 101, y: 0}) === false);
 
-assert(polygonPoints.boundingBox()[2].y === 100);
+assert(polygonPoints.boundingBox[2].y === 100);
 
 console.timeEnd('test 3');
 
@@ -71,11 +71,11 @@ console.timeEnd('test 3');
 
 vertexes = [{x: 0, y: 0}, {x: 100, y: 0}, {x: 100, y: 100}, {x: 0, y: 100}, {x: 50, y: 150}];
 
-polygonPoints = new PP(vertexes);
+polygonPoints.vertexes = vertexes;
 
 console.time('test 4');
 
-assert(polygonPoints.pointsLength() === 9184);
+assert(polygonPoints.pointsLength === 9184);
 
 assert(polygonPoints.containsPoint({x: 0, y: 0}) === true);
 
@@ -83,7 +83,7 @@ assert(polygonPoints.containsPoint({x: 10, y: 10}) === true);
 
 assert(polygonPoints.containsPoint({x: 101, y: 0}) === false);
 
-assert(polygonPoints.boundingBox()[2].y === 150);
+assert(polygonPoints.boundingBox[2].y === 150);
 
 console.timeEnd('test 4');
 
